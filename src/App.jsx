45d37071f09058b7163cb2ea3a69f91c704ae121
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-23 17:32:44 -0500

    Create basic heading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Heading from './components/Heading';
 
 function App() {
   return (
     <div className="bg-primary relative z-0 h-screen">
       <Navbar />
+      <Heading username="torrenegra" />
     </div>
   );
 }
---
+++ src/components/Heading.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+// eslint-disable-next-line react/prop-types
+const Heading = ({ username }) => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:3001/users/${username}`);
+        setUserData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, [username]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  console.log(userData);
+  return (
+    <div className="text-white">
+      {userData && (
+        <div>
+          <img src={userData.person.picture} alt="user-pic" />
+          <h2>{userData.person.name}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Heading;
