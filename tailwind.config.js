0aee4f17a486eb32281da484c22a493ae678d0e6
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-23 15:05:25 -0500

    Generate React project with Vite, Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030408",
+        secondary: "#aaa6c3",
+        tertiary: "#100f1c",
+      },
+      backgroundImage: {
+        "primary-background": "url('/src/assets/primary-background1.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
