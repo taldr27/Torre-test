343ec5e55cc4639af6b01a770cd80746a5b5fc0a
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-24 00:49:59 -0500

    Add clickable searchbar inside navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RxHamburgerMenu, RxMagnifyingGlass } from 'react-icons/rx';
 
-const Navbar = () => (
-  <nav className="flex justify-between w-full h-[72px] bg-primary text-white px-4 py-[21px] shadow-xl">
-    <div className="flex items-center gap-5">
-      <RxHamburgerMenu className="text-xl" />
-      <h2 className=" text-2xl">
-        torre
-        <span className="text-secondary">.co</span>
-      </h2>
-    </div>
-    <div className="flex items-center gap-5 text-xl">
-      <RxMagnifyingGlass />
-      <h2 className="text-secondary">SIGN IN</h2>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <nav className="flex justify-between w-full h-[72px] bg-primary text-white px-4 py-[21px] shadow-xl">
+      <div className="flex items-center gap-5">
+        <RxHamburgerMenu className="text-xl" />
+        <h2 className=" text-2xl">
+          torre
+          <span className="text-secondary">.co</span>
+        </h2>
+      </div>
+      <div className="flex items-center gap-5 text-xl">
+        {!showSearch && (
+          <button type="button" onClick={() => setShowSearch(!showSearch)}>
+            <RxMagnifyingGlass />
+          </button>
+        )}
+        {showSearch ? (
+          <input type="text" placeholder="Search..." />
+        ) : (
+          <h2 className="text-secondary">SIGN IN</h2>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
