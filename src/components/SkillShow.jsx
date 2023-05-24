a811b4fbb6a361bb175a91a11cdc26dce59d14f0
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-24 09:52:40 -0500

    Render data for default job inside the modal

--- src/components/SkillShow.jsx
+++ src/components/SkillShow.jsx
@@ -15,30 +15,57 @@ const SkillShow = ({
   );
 
   return (
-    <div className="fixed h-full bg-primary inset-0 z-50">
-      <nav className="flex items-center gap-3 py-5 px-4 text-xl shadow-xl text-white">
-        <CgClose onClick={handleOpen} className="text-2xl" />
+    <div className="fixed h-full bg-primary inset-0 z-50 xl:w-2/4 xl:h-[50%] xl:inset-auto">
+      <nav className="flex items-center gap-3 py-5 px-4 text-xl shadow-xl text-white mb-9">
+        <CgClose onClick={handleOpen} className="text-2xl cursor-pointer" />
         <h2>{selected.name}</h2>
       </nav>
-      <div className="flex flex-col">
-        <span>
-          Proficiency:
-          {selected.proficiency}
-        </span>
-        <span>
-          Recommendations:
-          {selected.recommendations}
-        </span>
-      </div>
-      <div>
-        Jobs with this skills
-        {filterJobs.length
-          ? (
-            <div>
-              <h2>{jobs.objective}</h2>
-            </div>
-          )
-          : <p>There are no jobs related with this skill</p>}
+      <div className="mx-4">
+        <div className="flex flex-col text-sm">
+          <span>
+            Proficiency:
+            {' '}
+            <span className="text-custom-gray">
+              {selected.proficiency}
+            </span>
+          </span>
+          <span>
+            Recommendations:
+            {' '}
+            <span className="text-custom-gray">
+              {selected.recommendations}
+            </span>
+          </span>
+        </div>
+        <div className="w-[100%] lg:w-[1px] border border-tertiary my-4 lg:mx-5 lg:min-h-screen" />
+        <div>
+          <span>
+            Jobs with this skill:
+          </span>
+          {filterJobs.length
+            ? (
+              <div className="h-16 text-custom-gray flex flex-col mt-5">
+                <span className="text-secondary">{jobs.objective}</span>
+                <span>
+                  <span>
+                    <span className="text-white">
+                      Status:
+                    </span>
+                    {' '}
+                    {jobs.status}
+                  </span>
+                </span>
+
+                <span>
+                  <span className="text-white">Language: </span>
+                  {jobs.languages[0].language.name}
+                  {' - '}
+                  {jobs.languages[0].fluency}
+                </span>
+              </div>
+            )
+            : <p>There are no jobs related with this skill</p>}
+        </div>
       </div>
     </div>
   );
