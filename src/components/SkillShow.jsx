04881eb438b14ea91682e95ddae548cf6a60703c
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-24 07:58:12 -0500

    Styling fix for skill buttons

---
+++ src/components/SkillShow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SkillShow = () => {
+  console.log('show');
+  return (
+    <div className="fixed h-full bg-red-500">
+      SkillShow
+    </div>
+  );
+};
+
+export default SkillShow;
--- src/components/SkillsData.jsx
+++ src/components/SkillsData.jsx
@@ -4,22 +4,25 @@ import { FaWeightHanging } from 'react-icons/fa';
 const SkillsData = ({ experienceLevel, dataArray }) => {
   const filter = dataArray.strengths.filter((strength) => strength.proficiency === experienceLevel);
   if (filter.length === 0) {
-    return <p>No skills founded</p>;
+    return <p>No skills found</p>;
   }
+
+  const handleShowSkill = () => {
+    console.log('button');
+  };
+
   return (
     <div className="flex flex-wrap gap-3 mt-3">
       {filter.map((filterStrength) => (
-        <div key={filterStrength.id}>
-          <div className="px-2 py-1 bg-tertiary rounded-xl text-sm flex items-center">
-            {filterStrength.name}
-            {filterStrength.weight ? (
-              <div className="flex items-center ml-2 gap-1">
-                <FaWeightHanging />
-                <span>{filterStrength.weight.toFixed(1)}</span>
-              </div>
-            ) : ''}
-          </div>
-        </div>
+        <button type="button" key={filterStrength.id} onClick={handleShowSkill} className="px-2 py-1 bg-tertiary rounded-xl text-sm flex items-center hover:bg-primary ">
+          {filterStrength.name}
+          {filterStrength.weight ? (
+            <div className="flex items-center ml-2 gap-1">
+              <FaWeightHanging />
+              <span>{filterStrength.weight.toFixed(1)}</span>
+            </div>
+          ) : ''}
+        </button>
 
       ))}
     </div>
