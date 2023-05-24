b75f4597c170377bc3ecab927c3f26aaa6d32b0f
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-23 20:17:06 -0500

    Send arrayData to skillsData component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import skillsArray from '../assets';
 import SkillsData from './SkillsData';
 
-const Skills = () => (
+const Skills = ({ dataArray }) => (
   <div>
     {
       skillsArray.map((skill) => (
         <div key={skill.name} className="flex">
           <img src={skill.img} alt={skill.img} className="w-8" />
           <span>{skill.name}</span>
-          <SkillsData experienceLevel={skill.name} />
+          <SkillsData experienceLevel={skill.name} dataArray={dataArray} />
         </div>
       ))
     }
--- src/components/SkillsData.jsx
+++ src/components/SkillsData.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 
 // eslint-disable-next-line react/prop-types
-const SkillsData = ({ experienceLevel }) => {
+const SkillsData = ({ experienceLevel, dataArray }) => {
   console.log(experienceLevel);
+  console.log(dataArray);
   return (
     <>
       <h1>UL</h1>
